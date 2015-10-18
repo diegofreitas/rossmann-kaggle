9cccc9339259aa5868f8776f1797a62c02e8f093
Diego Lins de Freitas <diego.freitas@fpf.br>
2015-10-18 17:56:49 -0400

    hash the store id split PromoInterval CompetitionOpen PromoOpen use RandomForestRegressor - rmspe 0.14

--- rossmann-store-sales/rossmann.py
+++ rossmann-store-sales/rossmann.py
@@ -7,6 +7,7 @@ import csv
 
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction import FeatureHasher
 from sklearn.cross_validation import train_test_split
 from sklearn import linear_model
 from sklearn.metrics import *
@@ -24,63 +25,93 @@ sh_encoder = LabelEncoder()
 ass_encoder = LabelEncoder()
 st_encoder = LabelEncoder()
 promo_interval_encoder = LabelEncoder()
+p1_encoder = LabelEncoder()
+p2_encoder = LabelEncoder()
+p3_encoder = LabelEncoder()
+p4_encoder = LabelEncoder()
 features_scaler = MinMaxScaler()
 features_scaler.fitted = False
+
+store_id_hasher = FeatureHasher(n_features=5, input_type='string', non_negative=True)
+store_id_hasher.fitted = False
 
 sales = MinMaxScaler()
 sales.fitted = False
 sale_means = None
 
-features = [ "DayOfWeek", "Open", "Promo", "StateHoliday",
-                "SchoolHoliday", "StoreType", "Assortment", "CompetitionDistance", "PromoInterval", "Store", "Promo2", 'Promo','Sales_Mean_Promo']
+#'Store_a', 'Store_b', 'Store_c', 'Store_d', 'Store_e',
+features = ['Store_a', 'Store_b', 'Store_c', 'Store_d', 'Store_e', "DayOfWeek", "Open", "Promo", "StateHoliday",
+            "SchoolHoliday", "StoreType", "Assortment", "CompetitionDistance", "PromoInterval", "Promo2", 'Promo','CompetitionOpen','PromoOpen','p_1','p_2','p_3','p_4',
+            'Sales_Mean_Promo', 'month', 'woy']
 
-categorical_features = [ header in ["DayOfWeek", "StateHoliday", "Assortment", "StoreType", "PromoInterval"] for header in features]
-print categorical_features
+categorical_features = [
+    header in ["DayOfWeek", "StateHoliday", "Assortment", "StoreType", "PromoInterval", 'month', 'woy','p_1','p_2','p_3','p_4'] for header in
+    features]
 one_hot_encoder = OneHotEncoder(categorical_features=categorical_features)
 
+
 def load_data(file="train.csv"):
-    global  sale_means
+    global sale_means
     df = pd.read_csv(file, dtype={
         'DayOfWeek': np.int,
         'Sales': np.float64,
         'Store': np.int,
         'SchoolHoliday': np.int
-    })
+    }, parse_dates=['Date'])
     store = pd.read_csv("store.csv", low_memory=False)
     df = pd.merge(df, store, on='Store')
-    #df.loc[(df.Open.isnull() & df.Sales > 0), 'Open'] = 1
+    # df.loc[(df.Open.isnull() & df.Sales > 0), 'Open'] = 1
     df.fillna(0, inplace=True)
+
+    df['year'] = df.Date.apply(lambda x: x.year)
+    df['month'] = df.Date.apply(lambda x: x.month)
+    df['woy'] = df.Date.apply(lambda x: x.weekofyear)
+
+    df['CompetitionOpen'] = 12 * (df.year - df.CompetitionOpenSinceYear) + (df.month - df.CompetitionOpenSinceMonth)
+    df['CompetitionOpen'] = df.CompetitionOpen.apply(lambda x: x if x > 0 else 0)
+
+    df['PromoOpen'] = 12 * (df.year - df.Promo2SinceYear) + (df.woy - df.Promo2SinceWeek) / float(4)
+    df['PromoOpen'] = df.PromoOpen.apply(lambda x: x if x > 0 else 0)
+
+    df['p_1'] = df.PromoInterval.apply(lambda x: x[:3] if type(x) == str else 0)
+    df['p_2'] = df.PromoInterval.apply(lambda x: x[4:7] if type(x) == str else 0)
+    df['p_3'] = df.PromoInterval.apply(lambda x: x[8:11] if type(x) == str else 0)
+    df['p_4'] = df.PromoInterval.apply(lambda x: x[12:15] if type(x) == str else 0)
 
     if not hasattr(sh_encoder, "classes_"):
         df = df[df["Open"] != 0]
         sh_encoder.fit(df["StateHoliday"])
-    if not hasattr(ass_encoder, "classes_"):
         ass_encoder.fit(df["Assortment"])
-    if not hasattr(st_encoder, "classes_"):
         st_encoder.fit(df["StoreType"])
-    if not hasattr(promo_interval_encoder, "classes_"):
         promo_interval_encoder.fit(df["PromoInterval"])
-    if not sales.fitted:
+        p1_encoder.fit(df['p_1'])
+        p2_encoder.fit(df['p_2'])
+        p3_encoder.fit(df['p_3'])
+        p4_encoder.fit(df['p_4'])
         df = df.loc[df.Sales > 0]
         sales.fit(df["Sales"])
         sales.fitted = True
-
+        store_id_hasher.fit_transform(df.Store.astype(np.str))
 
     df["StateHoliday"] = sh_encoder.transform(df["StateHoliday"])
     df["Assortment"] = ass_encoder.transform(df["Assortment"])
     df["StoreType"] = st_encoder.transform(df["StoreType"])
     df["PromoInterval"] = promo_interval_encoder.transform(df["PromoInterval"])
+    df["p_1"] = p1_encoder.transform(df["p_1"])
+    df["p_2"] = p2_encoder.transform(df["p_2"])
+    df["p_3"] = p3_encoder.transform(df["p_3"])
+    df["p_4"] = p4_encoder.transform(df["p_4"])
 
     if 'Sales' in df.columns:
         sale_means = df.groupby(['Store', 'DayOfWeek', 'Promo']).mean().Sales
         sale_means = sale_means.reset_index()
         sale_means.rename(columns={'Sales': 'Sales_Mean_Promo'}, inplace=True)
 
-    df = pd.merge(df, sale_means, on = ['Store','DayOfWeek','Promo'], how='left')
+    df = pd.merge(df, sale_means, on=['Store', 'DayOfWeek', 'Promo'], how='left')
     df.fillna(0, inplace=True)
 
 
-    #Test data set does not have  Sales
+    # Test data set does not have  Sales
     if not 'Sales' in df.columns:
         df["Sales"] = np.zeros(df.shape[0])
         #when test set is merged the order is messed up
@@ -89,45 +120,50 @@ def load_data(file="train.csv"):
     #sales_comptdistance = scaler.transform(df[["CompetitionDistance", "Sales"]].values)
 
     if not features_scaler.fitted:
-        features_scaler.fit(df[["CompetitionDistance","Sales_Mean_Promo"]])
+        features_scaler.fit(df[["CompetitionDistance", "Sales_Mean_Promo"]])
         features_scaler.fitted = True
-    df[["CompetitionDistance","Sales_Mean_Promo"]] = features_scaler.transform(df[["CompetitionDistance","Sales_Mean_Promo"]])
+
+    df[["CompetitionDistance", "Sales_Mean_Promo"]] = features_scaler.transform(
+        df[["CompetitionDistance", "Sales_Mean_Promo"]])
     df["Sales"] = sales.transform(df["Sales"])
 
-    print df.head()
+    store_id = store_id_hasher.transform(df.Store.astype(np.str)).toarray()
+    store_id_df = pd.DataFrame(columns=['Store_a', 'Store_b', 'Store_c', 'Store_d', 'Store_e'], data=store_id)
+    df = df.join(store_id_df)
 
-    if not hasattr(one_hot_encoder,"feature_indices_"):
+    print df.head()
+    if not hasattr(one_hot_encoder, "feature_indices_"):
         X = one_hot_encoder.fit(df[features].values)
     X = one_hot_encoder.transform(df[features].values).toarray()
     Y = df["Sales"]
-
     return X, Y
-
 
 X, Y = load_data()
 #poly = PolynomialFeatures(degree=2)
-#X = poly.fit_transform(X.values)
+#X = poly.fit_transform(X)
 
 Xtrain, Xtest, Ytrain, Ytest = train_test_split(X, Y, test_size=0.3, random_state=42)
 
 print "Trainning"
 #regressor = linear_model.Lasso()
-regressor = ExtraTreesRegressor(n_estimators=100)
+#regressor = ExtraTreesRegressor(n_estimators=100, warm_start=True)
 #regressor = linear_model.Ridge()
+regressor = RandomForestRegressor(n_estimators=20, warm_start=True, verbose=5, n_jobs=6)
+#regressor = KernelRidge() #Memory Error
+#regressor = GradientBoostingRegressor(n_estimators=10,warm_start=True, verbose=4)
 
-regressor.fit(Xtrain, Ytrain)
+#regressor.fit(Xtrain, Ytrain)
 
-predicted = regressor.predict(Xtest)
+#predicted = regressor.predict(Xtest)
 
-print "RMSPE", rmspe(sales.inverse_transform(Ytest), sales.inverse_transform(predicted))
+#print "RMSPE", rmspe(sales.inverse_transform(Ytest), sales.inverse_transform(predicted))
 
-#print "Refit"
-#regressor.fit(X, Y)
+print "Refit"
+regressor.fit(X, Y)
 
 X, Y = load_data("test.csv")
 
 predicted = sales.inverse_transform(regressor.predict(X))
-
 
 with open('submission_rossman.csv', 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
